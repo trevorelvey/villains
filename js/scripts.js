a36b6dad42aca41a0f33a24ee0b641b1e6fe647d
Brianna Popson and Trevor Elvey <bpopson@gmail.com>
2016-01-14 16:47:41 -0800

    fixed image stacking in villians.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   $("form#villains").submit(function(event) {
+    $("img").hide();
     var humorVariable = $("select#humor").val();
     var capeVariable = $("select#capes").val();
-    var result = ("");
     if (humorVariable === "anti-humor" && capeVariable === "anti-cape") {
       result = "President Snow";
       $("#snow").show();
 
     } else if (humorVariable === "pro-humor" && capeVariable === "pro-cape") {
       result = "Loki";
+    } if (result === "Loki"){
       $("#loki").show();
 
     } else if (humorVariable === "pro-humor" && capeVariable === "anti-cape") {
@@ -21,6 +22,7 @@ $(document).ready(function() {
     }
     $("#position").text(result);
     $("#result").show();
+
 
     event.preventDefault();
   });
