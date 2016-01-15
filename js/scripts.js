ebb9a3c1dfd38f5d16baee49dcc103b110cc9bc5
Brianna Popson and Trevor Elvey <bpopson@gmail.com>
2016-01-14 16:13:10 -0800

    built quiz page

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("form#villains").submit(function(event) {
+    var humorVariable = $("select#humor").val();
+    var capeVariable = $("select#capes").val();
+    var result = ("");
+    if (humorVariable === "anti-humor" && capeVariable === "anti-cape") {
+      result = "President Snow";
+      $("#snow").show();
+
+    } else if (humorVariable === "pro-humor" && capeVariable === "pro-cape") {
+      result = "Loki";
+      $("#loki").show();
+
+    } else if (humorVariable === "pro-humor" && capeVariable === "anti-cape") {
+      result = "Joker";
+      $("#joker").show();
+
+    } else if (humorVariable === "anti-humor" && capeVariable === "pro-cape") {
+        result = "Darth Vader";
+        $("#vader").show();
+    }
+    $("#position").text(result);
+    $("#result").show();
+
+    event.preventDefault();
+  });
+
+});
